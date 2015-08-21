38cb00eacdf6ac4424f8e1c81b0054bc88e9442f
Biljana Bajceta <biljanara@yahoo.com>
2015-08-21 07:12:15 +0200

    watch css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 var gulp = require('gulp');
+var launcher = require('simple-autoreload-server');
+ 
+var server = launcher({
+  port: 8000,
+  root: './root',
+  listDirectory: true,
+  watch: /\.(png|js|html|json|swf|css)$/i,
+  forceReload: [/\.json$/i, "static.swf"]
+});
 
-gulp.task('default',['autoprefixer']);
+gulp.task('default', ['autoprefixer','watch']);
+
+gulp.task('watch', function() {
+  gulp.watch(['**/main.css'], ['autoprefixer']);
+});
+
 
 gulp.task('autoprefixer', function() {
     var postcss = require('gulp-postcss');
